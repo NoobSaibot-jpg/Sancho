0264ad4d03b6fbb24a50b4189cdd89890941c43a
NoobSaibot-jpg <65870058+NoobSaibot-jpg@users.noreply.github.com>
2021-01-15 22:25:56 +0200

    Update pars.py

--- pars.py
+++ pars.py
@@ -32,21 +32,3 @@ def find_last():
     f = str(find_news.strip())
     find_date = soup.find('span', class_='pull-right').text
     return f'Вот последняя новость, которая была аж {find_date[17::]}\n\n'+ f
-
-
-def find_all():
-    find_news = soup.find('div', id='collapse1').text
-    f = str(find_news.strip())
-    find_date = soup.find('span', class_='pull-right').text
-    date = str(find_date)
-    date1= date[17::]
-    if date1 == c:
-        return 'Вот что мне удалось найти!\n\n'+ f
-    else:
-        return f'За сегодня новостей нет.\nПоследняя новость была опубликована {find_date[17::]}'
-
-def find_last():
-    find_news = soup.find('div', id='collapse1').text
-    f = str(find_news.strip())
-    find_date = soup.find('span', class_='pull-right').text
-    return f'Вот последняя новость, которая была аж {find_date[17::]}\n\n'+ f
