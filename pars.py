2e86665207625c4c276e08fd9636484ec9fa2ff4
NoobSaibot-jpg <65870058+NoobSaibot-jpg@users.noreply.github.com>
2021-02-04 21:19:53 +0200

    f

--- pars.py
+++ pars.py
@@ -4,20 +4,21 @@ from bs4 import BeautifulSoup as bs
 
 
 class News():
+    '''Класс поиска и проверки новостей'''
 
     def __init__(self, check):
         self.check = check
         with open (check, 'r', encoding= 'utf8'):
             pass
 
-
+    '''Считывание файла проверки'''
     def read_check(self):
         with open (self.check, 'r') as file:
             read = file.read()
             return read
 
 
-
+    '''Поиск новости для рассылки'''
     def check_news1(self):
         r = rq.get('https://bank.gov.ua/WebSelling/Home/News').text
         soup = bs(r, 'lxml')
@@ -25,6 +26,7 @@ class News():
         f = str(find_news.strip())
         return f
 
+    '''Ответ на кнопку последней новости'''
     def check_last(self):
         r = rq.get('https://bank.gov.ua/WebSelling/Home/News').text
         soup = bs(r, 'lxml')
@@ -32,6 +34,7 @@ class News():
         f = str(find_news.strip())
         return f'Вот последняя новость: \n\n{f} '
 
+    '''Проверка кол-ва новостей'''
     def check_point(self):
         r = rq.get('https://bank.gov.ua/WebSelling/Home/News').text
         soup = bs(r, 'lxml')
@@ -41,6 +44,7 @@ class News():
             a+=1
         return str(a)
 
+    '''Перезапись файла проверки'''
     def write_check_point(self):
         with open (self.check, 'w') as file:
             file.write(self.check_point())
